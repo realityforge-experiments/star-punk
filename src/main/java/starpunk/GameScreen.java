096ee1864e60272927a0f6bddf6f39f5bd5f03f7
Peter Donald <peter@realityforge.org>
2012-09-27 21:09:01 +1000

    Add minimal screen

---
+++ src/main/java/starpunk/GameScreen.java
@@ -0,0 +1,42 @@
+package starpunk;
+
+import com.badlogic.gdx.Screen;
+
+public class GameScreen
+  implements Screen
+{
+  @Override
+  public void render( final float delta )
+  {
+  }
+
+  @Override
+  public void resize( int width, int height )
+  {
+  }
+
+  @Override
+  public void show()
+  {
+  }
+
+  @Override
+  public void hide()
+  {
+  }
+
+  @Override
+  public void pause()
+  {
+  }
+
+  @Override
+  public void resume()
+  {
+  }
+
+  @Override
+  public void dispose()
+  {
+  }
+}
