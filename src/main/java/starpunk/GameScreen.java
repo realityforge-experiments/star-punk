cffcc450db8a8fa41c44c0c626918541bbb5ed84
Peter Donald <peter@realityforge.org>
2012-09-27 21:11:13 +1000

    Add a basic camera

--- src/main/java/starpunk/GameScreen.java
+++ src/main/java/starpunk/GameScreen.java
@@ -3,14 +3,23 @@ package starpunk;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 
 public class GameScreen
   implements Screen
 {
+  private OrthographicCamera _camera;
+  public GameScreen()
+  {
+    _camera = new OrthographicCamera( StarPunkGame.WIDTH, StarPunkGame.HEIGHT );
+  }
+
   @Override
   public void render( final float delta )
   {
     Gdx.gl10.glClear( GL10.GL_COLOR_BUFFER_BIT );
+
+    _camera.update();
   }
 
   @Override
