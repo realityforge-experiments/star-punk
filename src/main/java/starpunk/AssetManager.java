f2d5de53a560731476d96423988d2e55cdbdef32
Peter Donald <peter@realityforge.org>
2012-10-06 16:37:55 +1000

    Improve the dispose by disposing the containing atlas

--- src/main/java/starpunk/AssetManager.java
+++ src/main/java/starpunk/AssetManager.java
@@ -3,22 +3,31 @@ package starpunk;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import java.util.HashMap;
+import java.util.Map;
 
 public final class AssetManager
 {
   private final HashMap<String, TextureAtlas.AtlasRegion> _sprites = new HashMap<String, TextureAtlas.AtlasRegion>();
+  private final HashMap<String, TextureAtlas> _textures = new HashMap<String, TextureAtlas>();
 
   protected void initialize()
   {
-    final TextureAtlas textureAtlas = new TextureAtlas( Gdx.files.internal( "target/assets/game" ) );
+    final String textureName = "target/assets/game";
+    final TextureAtlas textureAtlas = new TextureAtlas( Gdx.files.internal( textureName ) );
     for( final TextureAtlas.AtlasRegion r : textureAtlas.getRegions() )
     {
       _sprites.put( r.name, r );
     }
+    _textures.put( textureName, textureAtlas );
   }
 
   protected void dispose()
   {
+    for( final Map.Entry<String, TextureAtlas> entry : _textures.entrySet() )
+    {
+      entry.getValue().dispose();
+    }
+    _textures.clear();
     _sprites.clear();
   }
 
