113c4e20dda456d9be568a83d8e2c40f3ca4cd87
Peter Donald <peter@realityforge.org>
2012-10-06 16:38:53 +1000

    Add In a font asset

--- src/main/java/starpunk/AssetManager.java
+++ src/main/java/starpunk/AssetManager.java
@@ -1,14 +1,20 @@
 package starpunk;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.stbtt.TrueTypeFontFactory;
 import java.util.HashMap;
 import java.util.Map;
 
 public final class AssetManager
 {
+  private static final String FONT_CHARACTERS =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789][_!$%#@|\\/?-+=()*&.;,{}\"´`'<>";
+
   private final HashMap<String, TextureAtlas.AtlasRegion> _sprites = new HashMap<String, TextureAtlas.AtlasRegion>();
   private final HashMap<String, TextureAtlas> _textures = new HashMap<String, TextureAtlas>();
+  private BitmapFont _font;
 
   protected void initialize()
   {
@@ -19,6 +25,14 @@ public final class AssetManager
       _sprites.put( r.name, r );
     }
     _textures.put( textureName, textureAtlas );
+
+    _font = TrueTypeFontFactory.createBitmapFont( Gdx.files.internal( "src/main/resources/psihoepatesaltern8.ttf" ),
+                                                  FONT_CHARACTERS,
+                                                  100,
+                                                  100,
+                                                  1.0f,
+                                                  StarPunkGame.WIDTH,
+                                                  StarPunkGame.HEIGHT );
   }
 
   protected void dispose()
@@ -29,6 +43,12 @@ public final class AssetManager
     }
     _textures.clear();
     _sprites.clear();
+    _font.dispose();
+  }
+
+  public BitmapFont getFont()
+  {
+    return _font;
   }
 
   public TextureAtlas.AtlasRegion getSprite( final String name )
