8408169a93c9817d09bb51ec9315ae460e4a70af
Peter Donald <peter@realityforge.org>
2012-10-27 15:29:51 +1100

    Add nullability annotations

--- src/main/java/starpunk/services/MusicReference.java
+++ src/main/java/starpunk/services/MusicReference.java
@@ -1,28 +1,34 @@
 package starpunk.services;
 
 import com.badlogic.gdx.audio.Music;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 public final class MusicReference
 {
+  @Nonnull
   private final String _filename;
+  @Nullable
   private Music _resource;
 
-  public MusicReference( final String filename )
+  public MusicReference( @Nonnull final String filename )
   {
     _filename = filename;
   }
 
+  @Nonnull
   public String getFilename()
   {
     return _filename;
   }
 
+  @Nullable
   Music getResource()
   {
     return _resource;
   }
 
-  void setResource( final Music resource )
+  void setResource( @Nullable final Music resource )
   {
     _resource = resource;
   }
