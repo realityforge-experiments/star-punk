68ce1433ba6bfa605e9acd98c77c1b744bcb351e
Peter Donald <peter@realityforge.org>
2012-10-27 12:40:27 +1100

    Add some basic logging

--- src/main/java/starpunk/StarPunkGame.java
+++ src/main/java/starpunk/StarPunkGame.java
@@ -1,6 +1,8 @@
 package starpunk;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
 import starpunk.screens.GameLoopScreen;
 
 public final class StarPunkGame
@@ -25,6 +27,7 @@ public final class StarPunkGame
   @Override
   public void create()
   {
+    Gdx.app.log( StarPunkGame.class.getSimpleName(), "Creating game on " + Gdx.app.getType() );
     _assetManager.initialize();
     setScreen( new GameLoopScreen( this ) );
   }
@@ -32,6 +35,7 @@ public final class StarPunkGame
   @Override
   public void dispose()
   {
+    Gdx.app.log( StarPunkGame.class.getSimpleName(), "Disposing game." );
     super.dispose();
     _assetManager.dispose();
   }
@@ -39,5 +43,33 @@ public final class StarPunkGame
   public AssetManager getAssetManager()
   {
     return _assetManager;
+  }
+
+  @Override
+  public void resize( final int width, final int height )
+  {
+    Gdx.app.log( StarPunkGame.class.getSimpleName(), "Resizing game to: " + width + " x " + height );
+    super.resize( width, height );
+  }
+
+  @Override
+  public void pause()
+  {
+    Gdx.app.log( StarPunkGame.class.getSimpleName(), "Pausing game" );
+    super.pause();
+  }
+
+  @Override
+  public void resume()
+  {
+    Gdx.app.log( StarPunkGame.class.getSimpleName(), "Resuming game" );
+    super.resume();
+  }
+
+  @Override
+  public void setScreen( final Screen screen )
+  {
+    Gdx.app.log( StarPunkGame.class.getSimpleName(), "Setting screen: " + screen.getClass().getSimpleName() );
+    super.setScreen( screen );
   }
 }
