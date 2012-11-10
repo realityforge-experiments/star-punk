278f48ff70fee9cc61e46fae1b776f23332ce61b
Peter Donald <peter@realityforge.org>
2012-11-10 18:17:00 +1100

    Remove texture packing as that is now done as part of the texture pipeline

--- src/main/java/starpunk/Main.java
+++ src/main/java/starpunk/Main.java
@@ -2,17 +2,11 @@ package starpunk;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.badlogic.gdx.tools.imagepacker.TexturePacker2;
 
 public class Main
 {
   public static void main( final String[] args )
   {
-    final TexturePacker2.Settings settings = new TexturePacker2.Settings();
-    settings.maxWidth = 1024;
-    settings.maxHeight = 1024;
-    TexturePacker2.process( settings, "src/main/resources", "target/assets", "game.atlas" );
-
     final LwjglApplicationConfiguration configuration = new LwjglApplicationConfiguration();
     configuration.fullscreen = false;
     configuration.width = StarPunkGame.WIDTH;
