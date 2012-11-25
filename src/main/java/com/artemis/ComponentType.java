ac497b87f9a39eb1a5bad8e2c405f97c54757891
Peter Donald <peter@realityforge.org>
2012-11-25 17:44:09 +1100

    Initial infrastructure to have  multiple components of the same type

--- src/main/java/com/artemis/ComponentType.java
+++ src/main/java/com/artemis/ComponentType.java
@@ -1,20 +1,26 @@
 package com.artemis;
 
 import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.Nonnull;
 
 public final class ComponentType
 {
-  private static final HashMap<Class<?>, ComponentType> _typeMap = new HashMap<Class<?>, ComponentType>();
+  private static final String DEFAULT_CLASSIFIER = "DEFAULT";
+  private static final HashMap<Class<?>, Map<String, ComponentType>> _typeMap =
+    new HashMap<Class<?>, Map<String, ComponentType>>();
 
   private static int c_maxIndex;
 
   private final int _index;
   private final Class<?> _type;
+  private final String _classifier;
 
-  private ComponentType( final Class<?> type )
+  private ComponentType( final Class<?> type, final String classifier )
   {
     _index = c_maxIndex++;
     _type = type;
+    _classifier = classifier;
   }
 
   public int getIndex()
@@ -25,18 +31,31 @@ public final class ComponentType
   @Override
   public String toString()
   {
-    return "ComponentType[" + _type.getSimpleName() + "] (" + _index + ")";
+    return "ComponentType[" + _type.getSimpleName() + "/" + _classifier + "] (" + _index + ")";
   }
 
-  public static ComponentType getTypeFor( final Class<?> c )
+  public static ComponentType getTypeFor( final Class<?> type )
   {
-    ComponentType type = _typeMap.get( c );
-    if( null == type )
+    return getTypeFor( type, DEFAULT_CLASSIFIER );
+  }
+
+  @Nonnull
+  public static ComponentType getTypeFor( @Nonnull final Class<?> type, @Nonnull final String classifier )
+  {
+    Map<String, ComponentType> componentTypeMap = _typeMap.get( type );
+    if( null == componentTypeMap )
     {
-      type = new ComponentType( c );
-      _typeMap.put( c, type );
+      componentTypeMap = new HashMap<String, ComponentType>();
+      _typeMap.put( type, componentTypeMap );
     }
-    return type;
+
+    ComponentType componentType = componentTypeMap.get( classifier );
+    if( null == componentType )
+    {
+      componentType = new ComponentType( type, classifier );
+      componentTypeMap.put( classifier, componentType );
+    }
+    return componentType;
   }
 
   public static int getIndexFor( final Class<?> c )
