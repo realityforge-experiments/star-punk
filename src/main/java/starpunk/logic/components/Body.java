3d31f3ac4e93f17c92e47db328693a8dbd767f1b
Peter Donald <peter@realityforge.org>
2012-11-25 12:04:34 +1100

    Deguff

--- src/main/java/starpunk/logic/components/Body.java
+++ src/main/java/starpunk/logic/components/Body.java
@@ -1,12 +1,5 @@
 package starpunk.logic.components;
 
-/**
- * Created with IntelliJ IDEA.
- * User: peter
- * Date: 25/11/12
- * Time: 9:30 AM
- * To change this template use File | Settings | File Templates.
- */
 public class Body
 {
 }
